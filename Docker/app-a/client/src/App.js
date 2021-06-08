0e87aa5ab7b5bbb40451b30b8cbc29256088baf5
BZIvanov <ivanov.biser@gmx.com>
2021-06-08 15:57:58 +0300

    app-a client setup

---
+++ Docker/app-a/client/src/App.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [usedPrices, setUsedPrices] = useState([]);
+  const [vats, setVats] = useState({});
+  const [price, setPrice] = useState('');
+
+  useEffect(() => {
+    const fetchUsedPrices = async () => {
+      const response = await axios.get('/prices/all');
+      setUsedPrices(response.data);
+    };
+
+    const fetchVats = async () => {
+      const response = await axios.get('/prices/current');
+      setVats(response.data);
+    };
+
+    fetchUsedPrices();
+    fetchVats();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post('/prices', { price });
+    setPrice('');
+  };
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            name='price'
+            type='number'
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <button type='submit'>Calculate</button>
+        </form>
+      </div>
+
+      <div>{JSON.stringify(usedPrices)}</div>
+
+      <div>{JSON.stringify(vats)}</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Docker/app-a/client/src/SuccessPage.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const SuccessPage = () => {
+  return (
+    <div>
+      <p>Success Page</p>
+      <Link to='/'>Back to Home</Link>
+    </div>
+  );
+};
+
+export default SuccessPage;
