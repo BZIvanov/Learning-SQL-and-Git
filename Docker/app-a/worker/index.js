0d9f6e5465a705210049b9be3cd5d59e6da26264
BZIvanov <ivanov.biser@gmx.com>
2021-06-08 14:25:01 +0300

    app-a worker setup

---
+++ Docker/app-a/worker/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  retry_strategy: () => 1000, // if we lose connection we will retry to connect every second
+});
+
+const sub = redisClient.duplicate();
+
+function calculateVAT(value) {
+  return value * 1.2;
+}
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, calculateVAT(parseInt(message, 10)));
+});
+
+sub.subscribe('insert');
