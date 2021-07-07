43634de253280f93f4db67fc8749b92085782795
BZIvanov <ivanov.biser@gmx.com>
2021-07-07 07:52:49 +0300

    user calcs exercixes updated with another column

--- SQL/exercises/user-calcs/solutions.sql
+++ SQL/exercises/user-calcs/solutions.sql
@@ -1,6 +1,7 @@
 SELECT 
 	concat(left(first_name, 1), '. ', last_name) AS fullName,
     income,
+    round(income) AS incomeRounded,
     (income * 2) AS doubledIncome,
     income / work_days AS incomePerDay
 FROM calculations;
